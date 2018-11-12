4c52150cd6914b5187a7a960d77667d59e0129b0
M-Morsy <mohamed.morsey96@gmail.com>
2018-11-12 10:31:12 +0200

    Add files via upload
    slight change in output

--- strategy pattern/Simulator.java
+++ strategy pattern/Simulator.java
@@ -9,11 +9,12 @@ public class Simulator {
     public static void main(String args[]){
         duck d1 = new HurtDuck();
         duck d2 = new toyDuck();
-        
+        System.out.println("Duck#0 not shot / hurt yet");
         d1.perform();
         d2.perform();
         
         // change the interface of d1 into FlyNoFly interface
+        System.out.println("Duck#0 got shot now");
         ((HurtDuck)d1).gotShot();
         d1.perform();
     }
